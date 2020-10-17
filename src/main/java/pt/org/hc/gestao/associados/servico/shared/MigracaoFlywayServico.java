84a68f8007ecc205b3c07a43aaeb2c3ae208402d
André Cleiser <andre.cleiser@trt6.jus.br>
2020-10-17 16:08:23 +0100

    mapper

--- src/main/java/pt/org/hc/gestao/associados/servico/shared/MigracaoFlywayServico.java
+++ src/main/java/pt/org/hc/gestao/associados/servico/shared/MigracaoFlywayServico.java
@@ -1,34 +1,27 @@
 package pt.org.hc.gestao.associados.servico.shared;
 
-import java.util.Optional;
-
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 
 import org.flywaydb.core.Flyway;
-import org.flywaydb.core.api.MigrationInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.quarkus.runtime.StartupEvent;
-import io.quarkus.runtime.configuration.ProfileManager;
 
-@ApplicationScoped
+// @ApplicationScoped
 public class MigracaoFlywayServico {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MigracaoFlywayServico.class);
+    // private static final Logger LOGGER = LoggerFactory.getLogger(MigracaoFlywayServico.class);
 
     public void aoIniciar(@Observes StartupEvent evento, Flyway flyway) {
-        LOGGER.info("A iniciar aplicação no modo: " + ProfileManager.getActiveProfile());
-        flyway.migrate();
-        Optional<MigrationInfo> migracaoCorrente = Optional.ofNullable(flyway.info().current());
+        // LOGGER.info("A iniciar aplicação no modo: " + ProfileManager.getActiveProfile());
+        // flyway.migrate();
+        // Optional<MigrationInfo> migracaoCorrente = Optional.ofNullable(flyway.info().current());
 
-        if (migracaoCorrente.isPresent()) {
-            LOGGER.info(migracaoCorrente.get().getVersion().getVersion());
-            LOGGER.info(migracaoCorrente.get().getState().toString());
-        } else {
-            LOGGER.info("Não há scripts para executar");
-        }
+        // if (migracaoCorrente.isPresent()) {
+        //     LOGGER.info(migracaoCorrente.get().getVersion().getVersion());
+        //     LOGGER.info(migracaoCorrente.get().getState().toString());
+        // } else {
+        //     LOGGER.info("Não há scripts para executar");
+        // }
     }
 
 }
--- target/generated-sources/annotations/pt/org/hc/gestao/associados/dto/mapper/AssociadoMapperImpl.java
+++ target/generated-sources/annotations/pt/org/hc/gestao/associados/dto/mapper/AssociadoMapperImpl.java
@@ -9,7 +9,7 @@ import pt.org.hc.gestao.associados.entidade.AssociadoFoto;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-10-16T17:39:04+0100",
+    date = "2020-10-17T08:43:55+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.7 (AdoptOpenJDK)"
 )
 @ApplicationScoped
